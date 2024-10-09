bf5f7030e0203489f41b024f89355ee696d7c1d1
Victorpraise5 <victorpraise949@gmail.com>
2024-10-09 12:36:43 +0000

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,5 @@ export default function getListStudentIds(arr) {
 	if (!Array.isArray(arr)) {
 		return [];
 	}
-
-	//using map: map is iterating on each element of the array and passes it to our function
 	return arr.map(student => student.id);
 }
